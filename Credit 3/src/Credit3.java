2b6c30bcd8f151b82b693996d4d4e3b351f75f66
904212004 <904212004@L815WD220008.edu.cbe.ab.ca>
2022-03-29 15:19:21 -0600

    03/29/2022

--- Credit 3/src/Credit3.java
+++ Credit 3/src/Credit3.java
@@ -1,9 +1,7 @@
-
-public class Credit3 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+public class 
+Credit3{
+	public static void main(String[] args){
+    System.out.println("Karan Gill 03/29/2022");
+  }
 }
+
---
+++ FromMeToYou/src/module-info.java
@@ -0,0 +1,8 @@
+public class
+FromMeToYou
+{
+public static void main(String args[])
+{
+	 System.out.println("Karan Gill 03/29/2022");
+}
+}
